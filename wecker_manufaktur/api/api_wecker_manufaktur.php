2786c7ea7f5b5e16e23ed638e8eb08b84339dff3
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2019-09-18 18:04:55 +0200

    corrected email address

--- wecker_manufaktur/api/api_wecker_manufaktur.php
+++ wecker_manufaktur/api/api_wecker_manufaktur.php
@@ -5,7 +5,7 @@ class api_wecker_manufaktur extends \SYSTEM\API\api_system {
     const EMAIL_NEWSLETTER_SUBSCRIBE    = 20;
 
     public static function call_send_mail($data){
-        $to = 'contact@democracy-deutschland.de';
+        $to = 'info@wecker-manufaktur.de';
         $data['json'] = str_replace('\/', '/',json_encode($data,JSON_PRETTY_PRINT));
         \SAI\saimod_mail::contact($data['email'],null,$data['name'],'');
         \SAI\saimod_mail::send_mail($to, self::EMAIL_WEBSITE_CONTACT, null,true,$data,$data['email']);
